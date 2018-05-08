fe52dc1e9b71f7b43b93443def3b78c36745f304
Exoshocker <39095563+Exoshocker@users.noreply.github.com>
2018-05-08 12:14:25 -0400

    Update RealWeekdayBot_2.py

--- RealWeekdayBot_2.py
+++ RealWeekdayBot_2.py
@@ -1,26 +1,36 @@
+#imports sleep function and PRAW, a package that manages the reddit API via python
 import praw
 from time import sleep
-
+#uses PRAW to log into a reddit account
 r = praw.Reddit(client_id='SASQjg-CXDy8-g',
                 client_secret='JdnLCd1DwsISCtb_yOASwoCqXG8',
                 password='jacksucks420',
                 username='WednesdayBot_1',
                 user_agent='Nice.')
+#prints "logged in" after completing the login, and only if the login is complete
 print("logged in")
 
-
+#creates an empty cache to store comment id's so that the script doesn't respond to the same comment twice in one run
 cache = []
-
+#sets Wednesday as a blank variable
 Wednesday = set()
+#sets a loop
 for i in range(0,10):
+  #tells the bot what subreddit to scan
     subreddit = r.subreddit('BackToSchoolDeals')
+    #tells the bot how many comments it can look at
     comments = subreddit.comments(limit = 20)
+    #starts the comment process
     for comment in comments:
+      #declares "body" as the body of the comments
         body = comment.body.lower()
+        #if the bot finds "Monday" in the comments, and doesn't find the unique comment ID in the cache I created at the beggining,
+        #it responds with a phrase, adds the comment ID to the cache, and relays that that day is done
         if ("monday" in body) and (comment.id not in cache):
             comment.reply("Not Good")
             cache.append(comment.id)
             print("Monday = Done")
+    #repeats the first section for every day of the week
     subreddit = r.subreddit('BackToSchoolDeals')
     comments = subreddit.comments(limit = 20)
     for comment in comments:
@@ -69,5 +79,6 @@ for i in range(0,10):
             comment.reply("Bad")
             cache.append(comment.id)
             print("Sunday = Done")
+    #puts the bot to sleep for 25 seconds before being able to run away  
     sleep(25)
         
